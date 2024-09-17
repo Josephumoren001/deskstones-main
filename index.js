2fc59c99403e350af835e073ffc0e8f13d906a7b
Josephumoren001 <josephumoren017@gmail.com>
2024-09-17 16:21:11 +0100

    server update

--- index.js
+++ index.js
@@ -34,10 +34,11 @@ app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 app.use('/api/mentor', MentorRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  // res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.json('hello world')
 });
 
 
